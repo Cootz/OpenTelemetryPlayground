72bf1d75bd1ff5bab56b8f78b6f5c261ccc797a3
Cootz <artem2004799@gmail.com>
2025-01-26 19:07:10 +0300

    Add project files.

---
+++ OpenTelemetryPlayground/Extensions.cs
@@ -0,0 +1,42 @@
+﻿using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace OpenTelemetryPlayground
+{
+    public static class Extensions
+    {
+        public static IHostApplicationBuilder ConfigureOpenTelemetry(this IHostApplicationBuilder builder)
+        {
+            builder.Logging.AddOpenTelemetry(openTelemetry =>
+            {
+                openTelemetry.IncludeScopes = true;
+                openTelemetry.IncludeFormattedMessage = true;
+            });
+
+            builder.Services.AddOpenTelemetry()
+                .ConfigureResource(resource => resource.AddService("WeatherForecast"))
+                .WithMetrics(metrics =>
+                {
+                    metrics
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation();
+
+                    metrics.AddPrometheusExporter();
+                })
+                .WithTracing(tracing =>
+                {
+                    tracing
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation();
+
+                    tracing.AddOtlpExporter();
+                });
+
+            builder.Logging.AddOpenTelemetry(loging => loging.AddOtlpExporter());
+            
+            return builder;
+        }
+    }
+}
